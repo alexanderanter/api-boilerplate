ecbc359a4c8eb44dbcceebda08f2f46e2fd8f47b
Beppe <beppe@live.se>
2018-07-19 00:08:11 +1000

    change dev db name

--- src/config/development.js
+++ src/config/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoose: {
-    uri: 'mongodb://database:27017/reftoken',
+    uri: 'mongodb://database:27017/db',
   },
   jwt: {
     secret: 'c563bbf30c548c177a76e0b4cfc95218cad80921',
