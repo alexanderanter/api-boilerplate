a71ca052f9c6dfbd584232972ba82cdfd3851427
Beppe <beppe@live.se>
2018-07-30 23:30:02 +1000

    adds encryption and token to user

--- models/User.js
+++ models/User.js
@@ -7,6 +7,12 @@ const schema = mongoose.Schema({
   picture: String,
   provider: String,
   providerId: String,
+  token: {
+    IV: Buffer,
+    encrypted: String,
+    tag: Buffer,
+    hash: String,
+  },
 });
 
 module.exports = mongoose.model('User', schema);
