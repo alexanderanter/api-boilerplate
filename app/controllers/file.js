eaa7553825bb9edc97d87b35c4bb1a2e7924c4ac
Beppe Karlsson <beppe@live.se>
2018-08-14 12:43:02 +1000

    throw better errors on file upload

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -1,9 +1,24 @@
 const upload = async (ctx, next) => {
   const { API_URL } = ctx.constants.CONFIG;
-  const { file } = ctx.request.body.files;
-  ctx.body = {
-    message: `Successfully uploaded file to ${API_URL}/${file.path}`,
-  };
+  try {
+    const { file } = ctx.request.body.files;
+    ctx.body = {
+      message: `Successfully uploaded file to ${API_URL}/${file.path}`,
+    };
+  } catch (error) {
+    const { BadRequest } = ctx.errors.ClientErrors;
+    const { InternalServerError } = ctx.errors.ServerErrors;
+    if (
+      error.message ===
+      `Cannot destructure property \`file\` of 'undefined' or 'null'.`
+    ) {
+      ctx.throw(new BadRequest());
+    } else if (error.message === `Cannot read property 'path' of undefined`) {
+      ctx.throw(new BadRequest());
+    } else {
+      ctx.throw(new InternalServerError());
+    }
+  }
 };
 
 const sendFile = async ctx => {
